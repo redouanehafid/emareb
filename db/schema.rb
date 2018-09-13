4b244d5e05a244370fdacb2ddd6f470c7eff049a
redouane <redouaanehafid@gmail.com>
2018-09-13 19:04:41 +0100

    add mysql db

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_09_11_165023) do
 
-  create_table "applications", force: :cascade do |t|
+  create_table "applications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nom"
     t.string "prenom"
     t.string "adresse"
@@ -33,14 +33,14 @@ ActiveRecord::Schema.define(version: 2018_09_11_165023) do
     t.datetime "lettre_updated_at"
   end
 
-  create_table "offres", force: :cascade do |t|
+  create_table "offres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
   end
 
-  create_table "projects", force: :cascade do |t|
+  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "client"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2018_09_11_165023) do
     t.datetime "image_updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
