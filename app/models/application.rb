b5a68e252ad19632a539584b91e299f1eb0390d9
redouane <redouaanehafid@gmail.com>
2018-09-20 21:31:26 +0100

    add carriere and styling pages

--- app/models/application.rb
+++ app/models/application.rb
@@ -6,4 +6,6 @@ class Application < ApplicationRecord
 	has_attached_file :lettre
 	validates_attachment :lettre, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
 
+	validates :nom, :prenom, :adresse, :codepostal, :ville, :telephone, :email, :message, :presence => true
+
 end
